932fe81785627a4b5dbda835d5686b8c6eb15b83
thereqwe@qq.com <3454453q>
2016-07-05 17:27:53 +0800

    123

--- responder.php
+++ responder.php
@@ -1,18 +1,43 @@
 <?
- require_once  "Mysql.php";
- $db = new MbMysql("30.97.16.232","root","123","micro_bang","","UTF8");
+error_reporting("E_ALL");
+require_once  "Mysql.php";
+ $db = new MbMysql("127.0.0.1","root","123","micro_bang","","UTF8");
  $action = p("action");
  if($action==""){
-   echo json_encode(array("errCode"=>"001","msg"=>"no action"));
+    echo j("001","action can not be empty");
  }
- if($action == "register"){
-  $name = p("name");
-  $pwd = p("pwd");
-  echo "$pwd $name";
+ /***********************************/
+ if($action == "register"){///////////////////////////
+    $name = p("name");
+    $pwd = p("pwd");
+    $rst = $db->query("select * from mb_member where name = '$name' ");
+    $num  = mysql_num_rows($rst);
+    if($num>0){
+        echo j("003","用户名已经被注册!");
+    }else{
+        $time = now();
+        $db->insert("mb_member","name,pwd,create_time","'$name','$pwd','$time'");
+        echo j("000","注册成功");
+    }
  }elseif($action== "login"){
-
+     $name = p("name");
+     $pwd = p("pwd");
+     $sql = "select * from mb_member where name = '$name' and pwd='$pwd'";
+     $rst = $db->query($sql);
+     $num  = mysql_num_rows($rst);
+     if($num>0) {
+         $rst = $db->fetch_array();
+         $arr = array("mid"=>$rst[0]["mid"]);
+         echo j("000", "登陆成功",$arr);
+     }else{
+         echo j("004", "用户名或密码错误");
+     }
+ }elseif($action== "logout"){
+ }elseif($action== "login1"){
+ }elseif($action== "login2"){
+ }elseif($action== "login3"){
  }else{
-   echo json_encode(array("errCode"=>"002","msg"=>"wrong action"));
+     echo j(null,"002","wrong action");
  }
 /////////////////////////////////////////////////////
 //get or post param
@@ -31,4 +56,23 @@ function safe($s){ //安全过滤函数
  if(get_magic_quotes_gpc()){ $s=stripslashes($s); }
  $s=mysql_real_escape_string($s);
  return $s;
+}
+
+function now()
+{
+  return  date('y-m-d h:i:s',time());
+}
+
+///json return
+function j($errCode="000",$msg="成功",$arr)
+{
+    $arr2 =  array(
+        "errCode"=>$errCode,
+        "msg"=>$msg
+    );
+    if($arr==null){
+        $arr = array();
+    }
+    $arr3=array_merge($arr,$arr2);
+    return json_encode($arr3);
 }
